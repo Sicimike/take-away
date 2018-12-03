a71271e27f11801b47a8060fd02ab9d0062eb038
Sicimike <877434251@qq.com>
2018-12-03 09:39:24 +0800

    config repository

--- config-server/src/main/java/com/sicimike/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/sicimike/config/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.sicimike.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author sicimike
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
